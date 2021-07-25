a293fc21506e594257819ad6c9f62fa5b6b0ff38
cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-25 18:57:43 +0100

    Setup handler to load and send json objects

--- main/server.c
+++ main/server.c
@@ -7,14 +7,30 @@
 #include "esp_http_server.h"
 #include "esp_wifi.h"
 #include "esp_log.h"
+#include "cJSON.h"
 
 #include "server.h"
 #include "config.h"
 
 const char *type = "text/html";
 
+static esp_err_t hello_get_handler(httpd_req_t *req)
+{
+    const char* resp_str = (const char*) req->user_ctx;
+    httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
+
+    if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
+        ESP_LOGI(TAG, "Request headers lost");
+    }
+    return ESP_OK;
+}
+
+static uint16_t volume = 35;
+static uint16_t val1 = 15;
+static uint16_t val2 = 52;
+
 /* An HTTP GET handler */
-static esp_err_t get_handler(httpd_req_t *req)
+static esp_err_t main_get_handler(httpd_req_t *req)
 {
     char*  buf;
     size_t buf_len;
@@ -31,49 +47,8 @@ static esp_err_t get_handler(httpd_req_t *req)
         free(buf);
     }
 
-    buf_len = httpd_req_get_hdr_value_len(req, "Test-Header-2") + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        if (httpd_req_get_hdr_value_str(req, "Test-Header-2", buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found header => Test-Header-2: %s", buf);
-        }
-        free(buf);
-    }
-
-    buf_len = httpd_req_get_hdr_value_len(req, "Test-Header-1") + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        if (httpd_req_get_hdr_value_str(req, "Test-Header-1", buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found header => Test-Header-1: %s", buf);
-        }
-        free(buf);
-    }
-
-    /* Read URL query string length and allocate memory for length + 1,
-     * extra byte for null termination */
-    buf_len = httpd_req_get_url_query_len(req) + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found URL query => %s", buf);
-            char param[32];
-            /* Get value of expected key from query string */
-            if (httpd_query_key_value(buf, "query1", param, sizeof(param)) == ESP_OK) {
-                ESP_LOGI(TAG, "Found URL query parameter => query1=%s", param);
-            }
-            if (httpd_query_key_value(buf, "query3", param, sizeof(param)) == ESP_OK) {
-                ESP_LOGI(TAG, "Found URL query parameter => query3=%s", param);
-            }
-            if (httpd_query_key_value(buf, "query2", param, sizeof(param)) == ESP_OK) {
-                ESP_LOGI(TAG, "Found URL query parameter => query2=%s", param);
-            }
-        }
-        free(buf);
-    }
-
     /* Set some custom headers */
-    httpd_resp_set_hdr(req, "Custom-Header-1", "Custom-Value-1");
-    httpd_resp_set_hdr(req, "Custom-Header-2", "Custom-Value-2");
+    httpd_resp_set_hdr(req, "Type", "Info");
 
     /* Send response with custom headers and body set as the
      * string passed in user context*/
@@ -88,22 +63,57 @@ static esp_err_t get_handler(httpd_req_t *req)
     return ESP_OK;
 }
 
+static esp_err_t update_get_handler(httpd_req_t *req){
+	httpd_resp_set_hdr(req, "Type", "Update");
+
+	/* Send response with custom headers and body set as the
+	* string passed in user context*/
+	/*char * val = strcat("{\"Volume\" : ",(char *)volume);
+	val = strcat(val,",\"val1\" :");
+	val = strcat(val,(char *)val1);
+	val = strcat(val,", \"val2\" : ");
+	val = strcat(val,(char *)val2);
+	val = strcat(val,"}");*/
+	cJSON *obj = cJSON_CreateObject();
+	cJSON_AddNumberToObject(obj, "Volume", volume);
+	cJSON_AddNumberToObject(obj, "val1", val1);
+	cJSON_AddNumberToObject(obj, "val2", val2);
+	char *objData = cJSON_PrintUnformatted(obj);
+
+	httpd_resp_send(req, objData, HTTPD_RESP_USE_STRLEN);
+
+	/* After sending the HTTP response the old HTTP request
+	* headers are lost. Check if HTTP request headers can be read now. */
+	if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
+	ESP_LOGI(TAG, "Request headers lost");
+	}
+	return ESP_OK;
+}
+
+
 static const httpd_uri_t hello = {
     .uri       = "/hello",
     .method    = HTTP_GET,
-    .handler   = get_handler,
+    .handler   = hello_get_handler,
     /* Let's pass response string in user
      * context to demonstrate it's usage */
     .user_ctx  = HTML_HELLO
 };
 
-static const httpd_uri_t hello = {
-    .uri       = "/info",
+static const httpd_uri_t info = {
+    .uri       = "/",
     .method    = HTTP_GET,
-    .handler   = get_handler,
+    .handler   = main_get_handler,
     /* Let's pass response string in user
      * context to demonstrate it's usage */
     .user_ctx  = HTML_MAIN
+};
+
+static const httpd_uri_t update = {
+    .uri       = "/update",
+    .method    = HTTP_GET,
+    .handler   = update_get_handler,
+	.user_ctx  = HTML_MAIN
 };
 
 static httpd_handle_t start_webserver(void)
@@ -118,6 +128,8 @@ static httpd_handle_t start_webserver(void)
         // Set URI handlers
         ESP_LOGI(TAG, "Registering URI handlers");
         httpd_register_uri_handler(server, &hello);
+        httpd_register_uri_handler(server, &info);
+        httpd_register_uri_handler(server, &update);
         return server;
     }
 
@@ -154,4 +166,5 @@ void init_server(void)
 	esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server);
 
 	server = start_webserver();
+	ESP_LOGI(TAG, "Server started.");
 }
