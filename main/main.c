87786f3bc2032621aafdd6f8eec5f6048969a55b
cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-25 02:10:34 +0100

    Setup initial scan for a given ssid

--- main/main.c
+++ main/main.c
@@ -1,42 +1,85 @@
-#include "freertos/FreeRTOS.h"
 #include "esp_wifi.h"
-#include "esp_system.h"
+#include "esp_log.h"
 #include "esp_event.h"
-#include "esp_event_loop.h"
+#include "string.h"
 #include "nvs_flash.h"
-#include "driver/gpio.h"
+#include "./webConfig.h"
 
-esp_err_t event_handler(void *ctx, system_event_t *event)
+#define SCAN_LIST_SIZE 15
+
+static const char* TAG = "ConnectionSetup";
+
+bool compareStrings(char* str1, char* str2)
 {
-    return ESP_OK;
+	//ESP_LOGI(TAG, "Checking -%s-", ssid);
+	int val = strcmp(str1, str2);
+	if (val == 0)
+	{
+		return true;
+	}
+	//ESP_LOGI(TAG, "Got -%d-", val);
+	return false;
+}
+
+void getWifiConnections()
+{
+	// Sets to STA mode
+	ESP_ERROR_CHECK(esp_netif_init());
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+	assert(sta_netif);
+
+	// Sets up default Config
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+	// Will store each record, init's to size of scan list
+	wifi_ap_record_t ap_info[SCAN_LIST_SIZE];
+	uint16_t ap_count = 0;
+	uint16_t num = SCAN_LIST_SIZE; // converts to uint16_t
+	memset(ap_info, 0, sizeof(ap_info));
+
+
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+	ESP_ERROR_CHECK(esp_wifi_start());
+
+	esp_wifi_scan_start(NULL, true); // start scanning
+	ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&num, ap_info)); // Get AP records, store into ap_info
+	ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count)); // Get total amount of items, store into ap_count
+	ESP_LOGI(TAG, "Total scanned \t\t%u", ap_count);
+	bool isFound = false;
+
+	for (int i = 0; (i < SCAN_LIST_SIZE) && (i < ap_count); i++)
+	{
+	        if (!isFound)
+	        {
+	        	isFound = compareStrings((char*)ap_info[i].ssid, WIFI_SSID);
+	        	if (isFound){
+	        		ESP_LOGI(TAG,"Found network!");
+	        		ESP_LOGI(TAG,"SSID \t\t%s", ap_info[i].ssid);
+	        		ESP_LOGI(TAG,"RSSI \t\t%d", ap_info[i].rssi);
+	        		ESP_LOGI(TAG,"Channel \t\t%d\n", ap_info[i].primary);
+	        	}
+	        }
+	}
+
+	if (isFound){
+		// do stuff
+	}else{
+		ESP_LOGI(TAG,"Network not found.");
+	}
 }
 
 void app_main(void)
 {
-    nvs_flash_init();
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = CONFIG_ESP_WIFI_SSID,
-            .password = CONFIG_ESP_WIFI_PASSWORD,
-            .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+	{
+	        ESP_ERROR_CHECK(nvs_flash_erase());
+	        ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK( ret );
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
-    while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
-    }
+	getWifiConnections();
 }
 
