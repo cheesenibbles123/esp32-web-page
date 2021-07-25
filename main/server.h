ca6434dc6e20bd91006e0224c99f4e3dc0bfa825
cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-25 18:57:23 +0100

    Adjusted html

--- main/server.h
+++ main/server.h
@@ -16,14 +16,36 @@
 			</body>\n\
 			</html>"
 
-#define HTML_MAIN "<!DOCTYPE HTML5>\n\
-			<html>\n\
-			<body>\n\
-			Random webpage!\n\
-			</body>\n\
-			</html>"
-
-static httpd_handle_t start_webserver(void);
+#define HTML_MAIN "<!DOCTYPE html>\
+<html lang=\"en-UK\">\
+<script type=\"text/javascript\">\
+	let ip = window.location.href.split(\"/\")[1];\
+	function updateLoop(){\
+		let data = fetch(`${ip}/update`).then(response => {\
+			if (response.status !== 200){\
+				return `Error code: ${response.status}`;\
+			}\
+			response.json().then(data => {\
+				return data;\
+			});\
+			return null;\
+		});\
+		let output = `Volume: ${data.Volume}\\nRandom value 1: ${data.val1}\\nRandom value 2: ${data.val2}`;\
+		document.getElementById(\"data\").innerHTML = output;\
+	}\
+	window.onload = function() {\
+		updateLoop();\
+		setInterval(()=>{\
+			updateLoop();\
+		},30000); // Every 10 sec\
+	}\
+</script>\
+<body>\
+<div id=\"data\">\
+	Getting Data....\
+</div>\
+</body>\
+</html>"
 
 void init_server(void);
 
